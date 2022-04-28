710bbc25fbf613921b16654b9e4f7af759bd54a5
sinanseyhan <seyhan.sinan@gmail.com>
2022-04-28 19:55:56 +0200

    Finished the 4th Challenge

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -8,10 +8,15 @@ from sklearn.pipeline import Pipeline
 from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
 from TaxiFareModel.utils import compute_rmse
 from TaxiFareModel.data import get_data, clean_data
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
 
 
 
 class Trainer():
+
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -20,6 +25,8 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.MLFLOW_URI = "https://mlflow.lewagon.ai/"
+        self.EXPERIMENT_NAME = "[DE][Berlin][SinanSeyhan]test_experiment_1"
 
 
 
@@ -41,14 +48,11 @@ class Trainer():
             ('preproc', preproc_pipe),
             ('linear_model', LinearRegression())
         ])
-        return self.pipeline
 
 
     def run(self, *X, **y):
         """set and train the pipeline"""
-
         self.pipeline.fit(self.X, self.y)
-        return self.pipeline
 
 
     def evaluate(self, X_test, y_test):
@@ -56,6 +60,33 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.EXPERIMENT_NAME)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.EXPERIMENT_NAME).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
+
 
 
 if __name__ == "__main__":
@@ -78,3 +109,6 @@ if __name__ == "__main__":
 
     # evaluate
     print(f'The RMSE score of the model is: {trainer.evaluate(X_test, y_test)}')
+
+    trainer.mlflow_log_param('estimator', trainer)
+    trainer.mlflow_log_metric('rmse', trainer.evaluate(X_test, y_test))
